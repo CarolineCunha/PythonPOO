bd76901fdec88769a2a94dac71e80216f2ae9527
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-08-22 07:28:49 -0300

    inicio projeto

--- conta.py
+++ conta.py
@@ -1,3 +1,7 @@
 class Conta:
-    def __init__(self):
+    def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite

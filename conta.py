1a53e1d6f92707a0a6d3fc6229affa4f61bce821
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-09-05 07:48:08 -0300

    métodos privados

--- conta.py
+++ conta.py
@@ -12,8 +12,14 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = (self.__saldo + self.__limite)
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else: print("O valor {} passou o limite".format(valor))
 
     def tranfere(self, valor, destino):
         self.saca(valor)
@@ -28,6 +34,7 @@ class Conta:
     @property
     def limite(self):
         return self.__limite
+
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
